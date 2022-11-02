a0ac74503efd2208dbd48fc6d1200475a46b6d81
Taifuuni <h3tulus@hotmail.com>
2022-11-02 19:12:01 +0200

    Add missing game stats tooltips

--- UI/Game Stats/StatsTooltipTexts.gd
+++ UI/Game Stats/StatsTooltipTexts.gd
@@ -29,35 +29,35 @@ var experience = {
 	"description": "Your current experience points."
 }
 
-#var strength = {
-#	"title": "Race",
-#	"description": "Your race determines your starting resistances."
-#}
-#
-#var race = {
-#	"title": "Race",
-#	"description": "Your race determines your starting resistances."
-#}
-#
-#var race = {
-#	"title": "Race",
-#	"description": "Your race determines your starting resistances."
-#}
-#
-#var race = {
-#	"title": "Race",
-#	"description": "Your race determines your starting resistances."
-#}
-#
-#var race = {
-#	"title": "Race",
-#	"description": "Your race determines your starting resistances."
-#}
-#
-#var race = {
-#	"title": "Race",
-#	"description": "Your race determines your starting resistances."
-#}
+var strength = {
+	"title": "Strength",
+	"description": "Strength determines your unarmed, two-handed and part of mace damage. Strength also affects carrying capacity."
+}
+
+var legerity = {
+	"title": "Legerity",
+	"description": "Legerity determines your dagger and flail damage and hit count. Higher legerity also improves HP regen and lowers stun turn count."
+}
+
+var balance = {
+	"title": "Balance",
+	"description": "Balance determines your sword and part of mace damage. Higher balance also gives higher maximum HP and lowers confusion and fumbling turn count."
+}
+
+var belief = {
+	"title": "Belief",
+	"description": "Belief determines your fleir damage. Higher belief also improves MP regen and lowers blindness turn count."
+}
+
+var visage = {
+	"title": "Visage",
+	"description": "Visage determines your frost and thunder damage. Higher visage also gives higher maximum MP."
+}
+
+var wisdom = {
+	"title": "Wisdom",
+	"description": "Wisdom determines your gleeie'er and toxix damage. Higher wisdom also lowers shop prices and invisibility turn count."
+}
 
 var hp = {
 	"title": "HP",
